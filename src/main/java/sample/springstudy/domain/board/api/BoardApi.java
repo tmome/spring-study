62fa886c301e3e3753c3c0f01fe754e371081f3c
천현승 <woqkfehlfk@naver.com>
2023-05-10 14:35:42 +0900

    게시물 조회 end point 추가

--- src/main/java/sample/springstudy/domain/board/api/BoardApi.java
+++ src/main/java/sample/springstudy/domain/board/api/BoardApi.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sample.springstudy.domain.board.dto.BoardPaginatedResponseDto;
@@ -33,5 +34,10 @@ public class BoardApi {
           direction = Direction.ASC) Pageable pageable
   ) {
     return ApiResponseGenerator.success(PageResponse.convert(boardService.getPaginatedBoard(pageable)));
+  }
+
+  @GetMapping("/{boardId}")
+  public ApiResponse<Object> getPost(final @PathVariable Long boardId) {
+    return ApiResponseGenerator.success(null);
   }
 }
