f4a87c63db1aa80148077f38d3a48d6c9e3b7b49
천현승 <woqkfehlfk@naver.com>
2023-05-12 16:31:16 +0900

    Api Exception handler structure 추가

---
+++ src/main/java/sample/springstudy/domain/global/handler/ApiExceptionHandler.java
@@ -0,0 +1,9 @@
+package sample.springstudy.domain.global.handler;
+
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+}
